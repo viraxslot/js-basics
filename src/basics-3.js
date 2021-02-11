33b6a2dbc97785e602e0f5f0eb48494cb7c2f5a0
Alex Vershinin <alexander.vershinin@kandasoft.com>
2021-02-11 20:34:19 +0300

    js basics 3

---
+++ src/basics-3.js
@@ -0,0 +1,10 @@
+// js basics 3: modules (require/import)
+import { calc } from './module.js';
+import { testData } from './test-data.js';
+// const _ = require('lodash');
+
+console.log(calc(testData.test));
+
+const arr = [1, 2, 3, 4];
+// const res = _.filter(arr, (el) => el > 2);
+// console.log(res);
---
+++ src/module.js
@@ -0,0 +1,5 @@
+const sqrt = (x) => x * x;
+
+export const sum = (x, y) => x + y;
+
+export const calc = (x) => sqrt(x);
